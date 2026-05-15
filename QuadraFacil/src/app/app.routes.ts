66d2cca925324ccce86180057439760758a821a4
jords342 <j0rd4ns4nt0s2021@gmail.com>
2026-05-15 10:19:14 -0300

    criação das páginas inicial e de login

--- QuadraFacil/src/app/app.routes.ts
+++ QuadraFacil/src/app/app.routes.ts
@@ -9,5 +9,13 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./pages/inicio/inicio.page').then( m => m.InicioPage)
+  },
+
 ];
