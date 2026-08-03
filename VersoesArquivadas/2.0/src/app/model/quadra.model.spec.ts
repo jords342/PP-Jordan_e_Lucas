6cff7eb399d5ff0aa51d422e66585cd4435602bf
Lukinhasf <lucasfialhodc@gmail.com>
2026-08-03 07:38:41 -0300

    remover versoes inuteles

--- VersoesArquivadas/2.0/src/app/model/quadra.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { QuadraModel } from './quadra.model';
-
-describe('QuadraModel', () => {
-  it('should create an instance', () => {
-    expect(new QuadraModel()).toBeTruthy();
-  });
-});
--- VersoesArquivadas/3.0/src/app/model/foto-quadra.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FotoQuadraModel } from './foto-quadra.model';
-
-describe('FotoQuadraModel', () => {
-  it('should create an instance', () => {
-    expect(new FotoQuadraModel()).toBeTruthy();
-  });
-});
