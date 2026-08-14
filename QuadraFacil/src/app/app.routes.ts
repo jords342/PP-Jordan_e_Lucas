e1239dde1d31bf9c5ae7c699bfe03e281d151bfe
jords342 <lucasfialhodc@gmail.com>
2026-08-14 09:58:36 -0300

    Pesquisa finalizada com sucesso

--- QuadraFacil/src/app/app.routes.ts
+++ QuadraFacil/src/app/app.routes.ts
@@ -43,6 +43,10 @@ export const routes: Routes = [
         loadComponent: () => import('./pages/meus-amigos/meus-amigos.page').then(m => m.MeusAmigosPage)
       },
       {
+        path: 'conversas',
+        loadComponent: () => import('./pages/conversas/conversas.page').then(m => m.ConversasPage)
+      },
+      {
         path: 'criar-quadra',
         loadComponent: () => import('./pages/criar-quadra/criar-quadra.page').then(m => m.CriarQuadraPage)
       },
@@ -61,6 +65,8 @@ export const routes: Routes = [
       { path: '', redirectTo: 'main', pathMatch: 'full' }
     ]
   },
+
+
 
 
 
